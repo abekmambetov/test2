5d9227c8cf46802f1778a4a6d0f1fb2dfe2972b0
abekmambetov <43209750+abekmambetov@users.noreply.github.com>
2021-09-01 00:51:57 +0400

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -5,17 +5,35 @@ if (!function_exists('getUrl')) {
     function getUrl($url){
         (function_exists('curl_init')) ? '' : die('cURL не установлен');
 
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; CrawlBot/1.0.0)');
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);    
-        curl_setopt($curl, CURLOPT_POST, true);
+        $ch = curl_init();
 
-        $html = curl_exec($curl);
-        $status = curl_getinfo($curl);
+        $options = array(
+            CURLOPT_URL            => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_ENCODING       => "",
+            CURLOPT_AUTOREFERER    => true,
+            CURLOPT_CONNECTTIMEOUT => 120,
+            CURLOPT_TIMEOUT        => 120,
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_SSL_VERIFYHOST => FALSE,
+            CURLOPT_SSL_VERIFYPEER => FALSE,
+            CURLOPT_USERAGENT => 'Mozilla/5.0 (compatible; CrawlBot/1.0.0)',
+            CURLOPT_POST => true,
+        );
 
-        curl_close($curl);
+        curl_setopt_array( $ch, $options );
+        $response = curl_exec($ch); 
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-        return array('html' => $html, 'status' => $status, 'request_method' => 'POST');     
+        if ( $httpCode != 200 ) {
+            return array('response' => '', 'httpCode' => $httpCode, 'request_method' => 'POST');    
+        } else {
+            return array('response' => $response, 'httpCode' => $httpCode, 'request_method' => 'POST');   
+        }
+
+        curl_close($ch);
+
+          
     }
 }
